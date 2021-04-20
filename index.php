bb05eb286e0f767d51654d8ba0f438221469d6df
ham8ani <ham8ani@gmail.com>
2021-04-20 11:46:54 +0700

    update 11:46wib

--- index.php
+++ index.php
@@ -71,7 +71,7 @@ require  'koneksi.php';
                             </div>
                             <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
                                 <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-                                Pages
+                                Pelaporan
                                 <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                             </a>
                             <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-parent="#sidenavAccordion">
