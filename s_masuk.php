c868e7a4912404f93413280dd64dc2606a09f756
ham8ani <ham8ani@gmail.com>
2021-04-23 14:55:31 +0700

    update 23 april 2021 pukul 14:55

--- s_masuk.php
+++ s_masuk.php
@@ -121,19 +121,11 @@
                         <ol class="breadcrumb mb-4">
                             <li class="breadcrumb-item active">Data Berdasarkan Surat Masuk</li>
                         </ol>
-                        <div class="row">                         
-                        </div>
-                        <div class="row">
-                            <div class="col-xl-6">
-                                <div class="card mb-4">
-                                
-                                </div>
-                           
-                            </div>
-                        </div>
+                        
+                        
                         <div class="card mb-4">
                             <div class="card-header">
-                                <i class="fas fa-table mr-1"></i>
+                                <svg class="svg-inline--fa fa-table fa-w-16 mr-1" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="table" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M464 32H48C21.49 32 0 53.49 0 80v352c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V80c0-26.51-21.49-48-48-48zM224 416H64v-96h160v96zm0-160H64v-96h160v96zm224 160H288v-96h160v96zm0-160H288v-96h160v96z"></path></svg><!-- <i class="fas fa-table mr-1"></i> Font Awesome fontawesome.com -->
                                 DataTable Example
                             </div>
                             <div class="card-body">
